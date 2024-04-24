2516cd1f1f81d4b07a0fc2d7d468083c79418b50
a <>
2024-04-24 12:34:08 -0700

    hmm, sha256 is 256 bits, not 256 bytes; which is 32 bytes

--- 99_c_main/src/d00_url.rs
+++ 99_c_main/src/d00_url.rs
@@ -7,13 +7,20 @@ pub struct Url {
 
 impl Url {
     pub fn to_256(&self) -> Url256 {
-        todo!("call sha 256")
+        // phind.com
+        use digest::Digest;
+        use sha2::Sha256;
+        let mut hasher = Sha256::new();
+        hasher.update(&self.target_url);
+        let result = hasher.finalize();
+        let out: [u8; 32] = result.try_into().expect("incorrect length");
+        Url256 { hash: out }
     }
 }
 
 #[derive(Eq, PartialEq, Hash, Copy, Clone)]
 pub struct Url256 {
-    hash: [u8; 256], // Birthday Paradox: For 2^64 urls, Pr collision < 2^(-128)
+    hash: [u8; 32], // Birthday Paradox: For 2^64 urls, Pr collision < 2^(-128)
 }
 
 pub struct ShardGroup<const N: usize> {
--- 99_c_main/src/d99_cluster_manager.rs
+++ 99_c_main/src/d99_cluster_manager.rs
@@ -1,12 +1,6 @@
 use super::*;
 
-// Manager
-
-/*
 pub struct ClusterManager {
-    check_fetched: HashMap<Node_CheckFetched_SG, std::net::Ipv4Addr>,
+    check_fetched: HashMap<Node_CheckFetchStatus_SG, std::net::Ipv4Addr>,
     urlmap: HashMap<Node_UrlMap_SG, std::net::Ipv4Addr>,
 }
-
-
- */
